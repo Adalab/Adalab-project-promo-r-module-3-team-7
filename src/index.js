6b1cc501a157969f1dbf6b7ec671ac708f8916a4
lvaldenebro <lauravaldeal@gmail.com>
2022-12-21 13:17:44 +0100

    Expands server.post in index.js to check wether the card is filled or not

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 // Importamos los dos módulos de NPM necesarios para trabajar
 const express = require('express');
 const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
 
 // Creamos el servidor
 const server = express();
-
+const saveCard = [];
 // Configuramos el servidor
 server.use(cors());
 server.use(express.json({limit: '25mb'}));
@@ -18,14 +19,27 @@ server.listen(serverPort, () => {
 //END POINTS
 server.post("/card", (req, res) => {
     //TODO: express II -> fake response
-    const responseSuccess = {
-        cardUrl: "https://dev.adalab.es/card/16715386762002278",
-        success: true
+    if (req.body.name === '' || req.body.job === '' || req.body.email === '' || req.body.phone === '' || req.body.github === '' || req.body.linkedin === '' || req.body.photo === '') {
+        const responseError = {
+            error: 'Faltan campos por rellenar',
+            success: false
+        }
+        res.json(responseError);
+    } else {
+        const newCard = {
+            id: uuidv4(),
+            ...req.body
+        }
+        saveCard.push(newCard);
+        const responseSuccess = {
+            cardURL: `http://localhost:4000/card/${newCard.id}`,
+            success: true
+        }
+        res.json(responseSuccess);
     }
-    res.json(responseSuccess);
 });
 
 //Draws card
-// server.get("card/id", (req, res) => {
+// server.get("card/${newCard.id}", (req, res) => {
 //  //TODO: express III
 // })
